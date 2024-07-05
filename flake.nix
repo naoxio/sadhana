0a8c5c79d2aa6ba974a9a2e6b1bdef67fe261d8f
M. Schiller <waotzi@proton.me>
2024-07-05 18:41:25 +0700

    attempt flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,21 @@
 {
   description = "Sadhana - A CLI tool for managing Sadhana practices";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
+        pkgsCross = pkgs.pkgsCross.mingwW64;
+
+        buildSadhana = { pkgs, static ? false }: pkgs.stdenv.mkDerivation {
           pname = "sadhana";
           version = "0.1.0";
           src = ./.;
-
           nativeBuildInputs = with pkgs; [ pkg-config ];
           buildInputs = with pkgs; [ tinycc termbox ];
-
           buildPhase = ''
             mkdir -p bin
             tcc -Wall -Wextra -pedantic -std=c99 \
@@ -27,29 +23,33 @@
                 -L${pkgs.termbox}/lib \
                 -o bin/sadhana \
                 src/*.c \
-                -ltermbox
+                -ltermbox \
+                ${if static then "-static" else ""}
           '';
-
           installPhase = ''
             mkdir -p $out/bin
             cp bin/sadhana $out/bin/
           '';
-
           meta = with pkgs.lib; {
             description = "A CLI tool for managing Sadhana practices";
-            homepage = "https://github.com/yourusername/sadhana";
-            license = licenses.mit;  # Adjust as needed
+            homepage = "https://github.com/naoxio/sadhana";
+            license = licenses.mit;
             platforms = platforms.all;
           };
         };
-
+      in
+      {
+        packages = {
+          default = buildSadhana { inherit pkgs; };
+          static = buildSadhana { inherit pkgs; static = true; };
+          windows = buildSadhana { pkgs = pkgsCross; static = true; };
+        };
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             tinycc
             pkg-config
             termbox
           ];
-
           shellHook = ''
             export PKG_CONFIG_PATH="${pkgs.termbox}/lib/pkgconfig:$PKG_CONFIG_PATH"
             export C_INCLUDE_PATH="${pkgs.termbox}/include:$C_INCLUDE_PATH"
@@ -59,4 +59,8 @@
         };
       }
     );
+    nixConfig = {
+      extra-substituters = [ "https://naoxio.cachix.org" ];
+      extra-trusted-public-keys = [ "naoxio.cachix.org-1:9+TVHBawUiQoy+GaxQOQzLYSfGnPdkjQqElBmGat2qo=" ];
+    };
 }
